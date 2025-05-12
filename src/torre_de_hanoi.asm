63fc0c3032120f23c5d38dd85284ab944a949d7a
Bruno Ramos <162474087+brunoramossilva@users.noreply.github.com>
2025-05-12 12:33:34 -0300

    Update torre_de_hanoi.asm

--- src/torre_de_hanoi.asm
+++ src/torre_de_hanoi.asm
@@ -1,5 +1,5 @@
-section .data ; Inicializando a seção que possui dados já definidos
-    prompt db "Digite um número: ", 0   ; Define a mensagem de entrada do prompt. O 0 no final serve para delimitar onde a string finalizará
+section .data ; Inicializando a seção que possui dados já definidos (como se fossem "constantes")
+    prompt db "Digite um número: ", 0   ; Define a mensagem de entrada do prompt. O 0 no final serve para delimitar onde a string finalizará (um "marco" para o programa saber que é para finalizar ali)
     len_prompt equ $ - prompt - 1       ; Calcula o tamanho da string, desconsiderando outros valores da memória e o próprio 0, que foi utilizado para delimitar o fim da string
     output db "Você digitou: ", 0       ; Define a mensagem de saída utilizando a mesma lógica da mensagem de entrada
     len_output equ $ - output - 1       ; Define o tamanho da mensagem de saída utilizando a mesma lógica da mensagem de entrada
