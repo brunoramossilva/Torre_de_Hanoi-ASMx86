15e85d17d9d2fed40d886f9d65d0ab50026c500c
Bruno Ramos <162474087+brunoramossilva@users.noreply.github.com>
2025-05-14 16:13:14 -0300

    Update torre_de_hanoi.asm
    Removi um bloco de código que estava sendo desnecessário para o intuito da aplicação

--- src/torre_de_hanoi.asm
+++ src/torre_de_hanoi.asm
@@ -1,9 +1,8 @@
 section .data ; Inicializando a seção que possui dados já definidos (como se fossem "constantes")
-    prompt db "Digite um número: ", 0 ; Define a mensagem de entrada do prompt. O 0 no final serve para delimitar onde a string finalizará (um "marco" para o programa saber que é para finalizar ali)
+    prompt db "Digite a quantidade de discos com que você deseja jogar: ", 0 ; Define a mensagem de entrada do prompt. O 0 no final serve para delimitar onde a string finalizará (um "marco" para o programa saber que é para finalizar ali)
     len_prompt equ $ - prompt - 1 ; Calcula o tamanho da string, desconsiderando outros valores da memória e o próprio 0, que foi utilizado para delimitar o fim da string
-    output db "Você digitou: ", 0 ; Define a mensagem de saída utilizando a mesma lógica da mensagem de entrada
+    output db "Quantidade de discos escolhida para jogar: ", 0 ; Define a mensagem de saída utilizando a mesma lógica da mensagem de entrada
     len_output equ $ - output - 1 ; Define o tamanho da mensagem de saída utilizando a mesma lógica da mensagem de entrada
-    newline db 10 ; Caractere de quebra de linha (ASCII 10 = "\n")
 
 section .bss ; Reserva espaço na memória para variáveis que serão usadas depois.
     num resb 2 ; Buffer para armazenar a entrada (até 2 bytes, o suficiente para a especificidade do projeto)
@@ -39,13 +38,6 @@ _start: ; Marca onde o código começará a ser executado
     mov ebx, 1 ; stdout (define o file descriptor [1 = saída padrão para a tela, stdout])
     mov ecx, num ; Endereço do buffer que contém o número
     mov edx, 2 ; Tamanho máximo a ser escrito (2 bytes)
-    int 0x80 ; Chama a interrupção do kernel (núcleo do SO) para executar a syscall
-
-    ; Adiciona uma nova linha
-    mov eax, 4 ; sys_write (código da syscall para escrever)
-    mov ebx, 1 ; stdout (define o file descriptor [1 = saída padrão para a tela, stdout])
-    mov ecx, newline ; Endereço do caractere (ASCII 10 = "\n")
-    mov edx, 1 ; Tamanho máximo a ser escrito (1 byte - 1 linha)
     int 0x80 ; Chama a interrupção do kernel (núcleo do SO) para executar a syscall
 
     ; Sai do programa
